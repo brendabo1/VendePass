dc864df68e5773bba76eced430b49a1b40b7850f
marcosviniciussena <marcvinisena@gmail.com>
2024-09-17 16:48:34 -0300

    .

--- Grafo/grafo.py
+++ Grafo/grafo.py
@@ -1,28 +1,31 @@
 import networkx as nx
 
 # Criar um grafo direcionado
-G = nx.Graph()
+G = nx.DiGraph()
 
-G.add_node("A", cidade="Salvador")
-G.add_node("B", cidade="Ilhéus")
-G.add_node("C", cidade="Porto Seguro")
-G.add_node("D", cidade="Lençois")
-G.add_node("E", cidade="Barreiras")
-G.add_node("F", cidade="Vitória da Conquista")
-G.add_node("G", cidade="Paulo Afonso")
-G.add_node("H", cidade="Feira de Santana")
-G.add_node("I", cidade="Guanambi")
-G.add_node("J", cidade="Teixeira de Freitas")
+G.add_node("SSA", cidade="Salvador")
+G.add_node("IOS", cidade="Ilhéus")
+G.add_node("BPS", cidade="Porto Seguro")
+G.add_node("LEC", cidade="Lençois")
+G.add_node("BRA", cidade="Barreiras")
+G.add_node("VDC", cidade="Vitória da Conquista")
+G.add_node("PAV", cidade="Paulo Afonso")
+G.add_node("FEC", cidade="Feira de Santana")
+G.add_node("GNM", cidade="Guanambi")
+G.add_node("TXF", cidade="Teixeira de Freitas")
 
 # Adicionar nós e arestas
 edges = [
-    ('A', 'B', 3), ('A', 'C', 3), ('A', 'D', 33), ('A', 'E', 3), ('A', 'F', 3), ('A', 'G', 3), ('A', 'H', 3), ('A', 'I', 3), ('A', 'J', 3),
-    ('B', 'C', 3), ('B', 'F', 5),
-    ('C', 'F', 3), ('C', 'J', 3),
-    ('D', 'E', 3), ('D', 'F', 5),('D', 'G', 3), ('D', 'H', 3),('D', 'I', 3),
-    ('E', 'G', 3), ('E', 'I', 3),('E', 'J', 3),
-    ('F', 'H', 3), ('F', 'I', 3),('F', 'J', 3),
-    ('G', 'H', 3), ('G', 'J', 3)
+    ('SSA', 'IOS', 33), ('SSA', 'BPS', 33), ('SSA', 'LEC', 33), ('SSA', 'BRA', 33), ('SSA', 'VDC', 33), ('SSA', 'PAV', 33), ('SSA', 'FEC', 33), ('SSA', 'GNM', 33), ('SSA', 'TXF', 33),
+    ('IOS', 'SSA', 5), ('IOS', 'BPS', 3), ('IOS', 'VDC', 5),
+    ('BPS', 'SSA', 3), ('BPS', 'IOS', 3), ('BPS', 'VDC', 3), ('BPS', 'TXF', 3),
+    ('LEC', 'SSA', 3), ('LEC', 'BRA', 3), ('LEC', 'VDC', 5), ('LEC', 'PAV', 3), ('LEC', 'FEC', 3),('LEC', 'GNM', 3),
+    ('BRA', 'SSA', 3), ('BRA', 'LEC', 3), ('BRA', 'PAV', 3), ('BRA', 'GNM', 3),('BRA', 'TXF', 3),
+    ('VDC', 'SSA', 3), ('VDC', 'IOS', 3), ('VDC', 'BPS', 3), ('VDC', 'LEC', 3), ('VDC', 'FEC', 3), ('VDC', 'GNM', 3),('VDC', 'TXF', 3),
+    ('PAV', 'SSA', 3), ('PAV', 'LEC', 3), ('PAV', 'BRA', 3), ('PAV', 'FEC', 3), ('PAV', 'TXF', 3),
+    ('FEC', 'SSA', 3), ('FEC', 'LEC', 3), ('FEC', 'VDC', 3), ('FEC', 'PAV', 3),
+    ('GNM', 'SSA', 3), ('GNM', 'LEC', 3), ('GNM', 'BRA', 3), ('GNM', 'VDC', 3),
+    ('TXF', 'SSA', 3), ('TXF', 'BPS', 3), ('TXF', 'VDC', 3)
 ]
 
 # Definir a propriedade 'vaga' nas arestas
@@ -34,14 +37,23 @@ for u, v, weight in edges:
     G.add_edge(u, v, weight=weight, vaga=5, assentos=["1A", "1B", "2A", "2B"])
 
 # Encontrar o caminho mais curto de A para G
-caminho = nx.shortest_path(G, source='B', target='D')
+caminho = nx.shortest_path(G, source='TXF', target='BRA')
 
-comprimento = nx.shortest_path_length(G, source='B', target='D')
+comprimento = nx.shortest_path_length(G, source='TXF', target='BRA')
 
-caminho_dijkstra = nx.dijkstra_path(G, source='B', target='D', weight='weight')
+caminho_dijkstra = nx.dijkstra_path(G, source='TXF', target='BRA', weight='weight')
 
-distancia_dijkstra = nx.dijkstra_path_length(G, source='B', target='D', weight='weight')
+distancia_dijkstra = nx.dijkstra_path_length(G, source='TXF', target='BRA', weight='weight')
 
+# Função para calcular o peso total de um caminho
+def path_weight(graph, path):
+    weight = 0
+    for i in range(len(path) - 1):
+        # Soma o peso da aresta entre os nós consecutivos no caminho
+        weight += graph[path[i]][path[i + 1]].get('weight', 0)
+    return weight
+
+comprimento_ponderado = path_weight(G, caminho)
 
 # Atualizar a propriedade 'vaga' ao longo do caminho
 for i in range(len(caminho) - 1):
@@ -54,7 +66,9 @@ for i in range(len(caminho) - 1):
 for u, v in G.edges:
     print(f"Aresta ({G.nodes[u]['cidade']}, {G.nodes[v]['cidade']}) tem a propriedade 'vaga': {G[u][v]['vaga']}")
 
+print(caminho)
 print(comprimento)
+print(comprimento_ponderado)
 print(caminho_dijkstra)
 print(distancia_dijkstra)
 
@@ -64,3 +78,4 @@ for i in range(len(caminho_dijkstra) - 1):
     if 'assentos' in G[u][v]:
         G[u][v]['assentos'].remove('1A') # Diminui a vaga, mas não deixa o valor negativo
         print(f"Rota {u} à {v}, assentos disponiveis: {G[u][v]['assentos']}")
+ 
