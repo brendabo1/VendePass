b14f4a4cb3607b8054b01f1d7440f3a6812d036c
marcosviniciussena <marcvinisena@gmail.com>
2024-09-19 14:16:01 -0300

    Menu

--- Cliente/app_cliente.py
+++ Cliente/app_cliente.py
@@ -28,50 +28,67 @@ class Cliente:
     
     def solicitar_rotas(self):
         requisicao = {"tipo": "listar_rotas"}
-        self.client_socket.sendall(json.dumps(requisicao).encode())
-        rotas = json.loads(self.client_socket.recv(1024).decode())
+        self.__client_socket.sendall(json.dumps(requisicao).encode())
+        rotas = json.loads(self.__client_socket.recv(1024).decode())
         return rotas
     
     def comprar_passagem(self, origem, destino, assento):
         requisicao = {"tipo": "comprar_passagem", "origem": origem, "destino": destino}
-        self.client_socket.sendall(json.dumps(requisicao).encode())
+        self.__client_socket.sendall(json.dumps(requisicao).encode())
 
-        resposta = json.loads(self.client_socket.recv(1024).decode())
+        resposta = json.loads(self.__client_socket.recv(1024).decode())
         if resposta["status"] == "sucesso":
             print(resposta["mensagem"])
             print("Assentos disponíveis: ", resposta["assentos_disponiveis"])
 
             requisicao_assento = {"assento": assento}
-            self.client_socket.sendall(json.dumps(requisicao_assento).encode())
+            self.__client_socket.sendall(json.dumps(requisicao_assento).encode())
             
-            resposta_final = json.loads(self.client_socket.recv(1024).decode())
+            resposta_final = json.loads(self.__client_socket.recv(1024).decode())
             return resposta_final
         else:
             return resposta
 
     def fechar_conexao(self):
-        self.client_socket.close()
+        self.__client_socket.close()
 
 if __name__ == "__main__":
     cliente = Cliente()
     cliente.conectar()
 
+    menu = True
 
-    # Solicitar rotas disponíveis
-    rotas = cliente.solicitar_rotas()
-    print("Rotas disponíveis:")
-    for rota in rotas:
-        status = "Disponível" if rota["disponivel"] else "Indisponível"
-        print(f'{rota["origem"]} -> {rota["destino"]}: {status} - Preço: {rota["preco"]}')
+    while menu:
+        print("===== MENU =====")
+        print("1- Comprar passagem")
+        print("2- Sair")
 
-    # Solicitar compra de passagem
-    origem = input("Escolha a origem: ")
-    destino = input("Escolha o destino: ")
-    assento = input("Escolha o assento: ")
+        menuOption = input("Digite a opção do menu: ")
+    
+            
+        if menuOption == '1':
+            rotas = cliente.solicitar_rotas()
+            print("Rotas disponíveis:")
+            for rota in rotas:
+                status = "Disponível" if rota["disponivel"] else "Indisponível"
+                print(f'{rota["origem"]} -> {rota["destino"]}: {status} - Preço: {rota["preco"]}')
 
-    resposta_final = cliente.comprar_passagem(origem, destino, assento)
-    print(resposta_final["mensagem"])
-    if resposta_final["status"] == "sucesso":
-        print(f"Preço: {resposta_final['preco']}")
+            # Solicitar compra de passagem
+            origem = input("Escolha a origem: ")
+            destino = input("Escolha o destino: ")
+            assento = input("Escolha o assento: ")
 
-    cliente.fechar_conexao()
+            resposta_final = cliente.comprar_passagem(origem, destino, assento)
+            print(resposta_final["mensagem"])
+            if resposta_final["status"] == "sucesso":
+                print(f"Preço: {resposta_final['preco']}")
+
+        elif menuOption == "2":
+            print("SAINDO")
+            cliente.fechar_conexao()
+            menu = False
+        else:
+            print("DIGITE UM VALOR VÁLIDO")
+    
+    print("FIM DO PROGRAMA")
+
